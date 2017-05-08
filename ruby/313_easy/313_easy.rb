f275aa5afd8b6fa35ac9310d6c809e7e06cee9f2
Martin McGrail <mmcgrail@epoch.com>
2017-05-08 10:09:07 -0700

    313 Easy: final solution for base challenge.

--- ruby/313_easy/313_easy.rb
+++ ruby/313_easy/313_easy.rb
@@ -1,17 +1,17 @@
 # [2017-05-01] Challenge #313 [Easy] Subset sum
-# link
+# https://redd.it/68oda5
 
-# brute force version
+# base challenge
 
-input = [1, 2, 3, 4, 5, -5]
+input = [0, 1, 2, 3, 4, 5]
 
 # update all elements to absolute
 input.map! {|i| i.abs }
 
 # compare arrays
-if input.uniq.length != input.length
-  puts "found dups: true"
+if (input.uniq.length != input.length) || (input.any? {|i| i == 0})
+  puts "true -- is subset, found dups or zero."
 else
-  puts "no dups: false"
+  puts "false -- not subset, found no dups or zero."
 end
 
