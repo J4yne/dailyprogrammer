a864f5df0090cbe1eb664b7fa72a61e0790d04f7
Martin McGrail <mmcgrail@epoch.com>
2017-05-30 16:02:47 -0700

    completed 317 easy, no bonus.

--- ruby/317_easy/317_easy.rb
+++ ruby/317_easy/317_easy.rb
@@ -1,17 +1,30 @@
-# 317 easy challenge
+# [2017-05-29] Challenge #317 [Easy] Collatz Tag System
+# https://redd.it/6e08v6
+
+def compute_collatz(input)
+  until input == "a" do
+    # slice out the first 2 digits.
+    s = input.slice!(0..1)
+
+    # test first digit of slice, and append relevant string.
+    if s.chr == "a"
+      input << "bc"
+    elsif s.chr == "b"
+      input << "a"
+    else # it's "c"
+      input << "aaa"
+    end
+
+    # print result of current iteration
+    puts "#{input}"
+  end
+  
+  puts "\n" # just separating inputs
+end
 
 input = "aaa"
+compute_collatz(input)
 
-until input == "a" do
-  x = input.slice!(0..2)
-  if input.start_with?("a")
-    input = input + "bc"
-  elsif input.start with?("b")
-    input = input + "a"
-  else
-    input = input + "aaa"
-  end
-
-  puts "#{input}"
-end
+input = "aaaaaaa"
+compute_collatz(input)
 
