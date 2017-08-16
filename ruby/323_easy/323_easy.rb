b78e5c8f0520b5085c1c01ca004ea96f22cb7744
Martin McGrail <mmcgrail@epoch.com>
2017-08-16 16:04:23 -0700

    323 Easy: added array to remove duplicate sets, per challenge output.

--- ruby/323_easy/323_easy.rb
+++ ruby/323_easy/323_easy.rb
@@ -6,20 +6,26 @@ input1 = [4, 5, -1, -2, -7, 2, -5, -3, -7, -3, 1]
 input2 = [-1, -6, -3, -7, 5, -8, 2, -8, 1]
 input3 = [-5, -1, -4, 2, 9, -9, -6, -1, -7]
 
-# method to find combos
-def find_zero_combos(set)
-  set.combination(3) { |c|
-    if c.sum == 0
-      puts "#{c}"
+def find_triples(set)
+
+  trips = [] # array for finding dups
+ 
+  set.combination(3) { |combo|
+    if combo.sum == 0
+      trips << combo # dump good combo into array
     else
       next
     end
   }
-  puts "\n" # spacing output
+
+  trips.each { |i| i.sort! } # sort array, then
+  trips.uniq! # remove dups, then
+  trips.each { |i| puts "#{i}" } # print em out
+  puts "\n" # spacer
 end
 
 # output
-find_zero_combos(input1)
-find_zero_combos(input2)
-find_zero_combos(input3)
+find_triples(input1)
+find_triples(input2)
+find_triples(input3)
 
